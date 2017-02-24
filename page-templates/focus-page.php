e9904932a0f14f3492c7b642b0913924b61dca05
Cole Bessee <cbessee@uw.edu>
2017-02-24 15:21:12 -0800

    clean up comments, tags, and other default wp stuff we don't need

--- page-templates/focus-page.php
+++ page-templates/focus-page.php
@@ -26,15 +26,10 @@
        <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
        <div class="entry-content">
            <?php the_content(); ?>
-           <?php edit_post_link( __( 'Edit', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
        </div>
        <footer>
            <?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
-           <p><?php the_tags(); ?></p>
        </footer>
-       <?php do_action( 'foundationpress_page_before_comments' ); ?>
-       <?php comments_template(); ?>
-       <?php do_action( 'foundationpress_page_after_comments' ); ?>
    </article>
  <?php endwhile;?>
 
