1dd05a0967cba6bfe88338ec062bc78112c0493b
Cole Bessee <cbessee@uw.edu>
2017-03-14 14:39:53 -0700

    close off canvas wrapper

--- footer.php
+++ footer.php
@@ -61,6 +61,11 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         
+		<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
+				</div><!-- Close off-canvas wrapper inner -->
+			</div><!-- Close off-canvas wrapper -->
+		</div><!-- Close off-canvas content wrapper -->
+		<?php endif; ?>
         <?php wp_footer() ?>
     </body>
 </html>
