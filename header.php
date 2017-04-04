d72e498c4812b8b2d27d393eb2c3649c9fb9fe46
Cole Bessee <cbessee@uw.edu>
2017-04-04 12:41:49 -0700

    mobile menu toggle fix

--- header.php
+++ header.php
@@ -153,7 +153,7 @@
 			</div>
 		</div>
 
-		<div class="title-bar" data-responsive-toggle="mobile-menu">
+		<div class="title-bar" data-responsive-toggle="site-navigation">
 			<button class="menu-icon" type="button" data-toggle="mobile-menu"><span class="hide">Mobile Menu</span></button>
 			<div class="title-bar-title">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
