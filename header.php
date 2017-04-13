864e823f47ef9ac64b2651d6ae65f3b745da5a56
Cole Bessee <cbessee@uw.edu>
2017-04-13 10:23:46 -0700

    add google analytics tracking code

--- header.php
+++ header.php
@@ -77,6 +77,17 @@
 		<meta name="msapplication-TileColor" content="#4b2e84">
 		<meta name="theme-color" content="#4b2e84">
 
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-97311419-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
