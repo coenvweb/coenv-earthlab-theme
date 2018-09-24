03fbe55f263138ccc9a9a7201c9304c4b4e1881e
drewcoll <drewcoll@gmail.com>
2018-09-24 16:28:08 -0700

    real sticky post fix

--- home.php
+++ home.php
@@ -89,7 +89,7 @@ Template Name: Homepage
                         $query_args = array(
                             'post_type' => 'post',
                             'post_status' => 'publish',
-                            'posts_per_page' => 1,
+                            'posts_per_page' => 2,
                             'post__in'  => get_option( 'sticky_posts' ),
                             'ignore_sticky_posts' => 1,
                         );
