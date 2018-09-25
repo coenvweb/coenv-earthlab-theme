aa9ad4b0c1c13e3b9356f46a9526feb9d3ac4d1a
drewcoll <drewcoll@gmail.com>
2018-09-24 17:05:26 -0700

    cont

--- template-parts/article.php
+++ template-parts/article.php
@@ -25,7 +25,7 @@
 
                     foreach ($more_terms as &$term) {
                         if(get_post_type() == 'post') {
-                            $more_terms_arr[] = '<a href="' . site_url() . '/about/news-and-events/topic/' . $term->slug . '">' . $term->name . '</a>';
+                            $more_terms_arr[] = '<a href="' . site_url() . '/news-and-events/topic/' . $term->slug . '">' . $term->name . '</a>';
                         }
                         if(get_post_type() == 'project') {
                             $more_terms_arr[] = '<a href="' . site_url() . '/projects/topic/' . $term->slug . '">' . $term->name . '</a>';
