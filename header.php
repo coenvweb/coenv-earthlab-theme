79cec246d2413fe0a287d95b9f0f0a811d84aa60
drewcoll <drewcoll@gmail.com>
2021-08-30 14:33:28 -0700

    test change

--- header.php
+++ header.php
@@ -156,7 +156,6 @@
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-
         gtag('config', 'G-NLLG4VWYZN');
         </script>
 
