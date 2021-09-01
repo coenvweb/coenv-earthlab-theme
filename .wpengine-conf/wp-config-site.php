96ac64cb7fff3f8c86c648dfa754ad42beb21eae
drewcoll <drewcoll@gmail.com>
2021-09-01 13:54:13 -0700

    testing earthlab repo

---
+++ .wpengine-conf/wp-config-site.php
@@ -0,0 +1,5 @@
+<?php
+# GENERATED AUTOMATICALLY ON 2021-03-03 15:42:00.237186 -0800 PST m=+252.535988953
+$table_prefix = "wp_";
+$wpengine_primary_original_domain = "uwearthlabdev.wpengine.com";
+$wpengine_primary_original_ip_address = "34.75.155.76";
--- index.php
+++ index.php
@@ -1,49 +1,17 @@
 <?php
 /**
- * The main template file
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * e.g., it puts together the home page when no home.php file exists.
- *
- * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
- *
- * @package FoundationPress
- * @since FoundationPress 1.0.0
+ * @package WordPress
  */
 
-get_header(); ?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-<div id="page-sidebar-left" role="main">
-	<article class="main-content">
-	<?php if ( have_posts() ) : ?>
-
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-		<?php endwhile; ?>
-
-		<?php else : ?>
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; // End have_posts() check. ?>
-
-		<?php /* Display navigation to next/previous pages when applicable */ ?>
-		<?php
-		if ( function_exists( 'foundationpress_pagination' ) ) :
-			foundationpress_pagination();
-		elseif ( is_paged() ) :
-		?>
-			<nav id="post-nav">
-				<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-			</nav>
-		<?php endif; ?>
-
-	</article>
-	<?php get_sidebar(); ?>
-
-</div>
-
-<?php get_footer();
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
