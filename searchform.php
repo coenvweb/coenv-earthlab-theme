4d4c9f57d1e36af330ccd0277a912c3ac8af98cc
drewcoll <drewcoll@gmail.com>
2026-02-20 15:43:49 -0800

    fix search form and social button aria labeling

--- searchform.php
+++ searchform.php
@@ -4,6 +4,6 @@
         <input type="hidden" name="post_type" value="<?php get_query_var('post_type') ?>" />
     <?php } ?>
 	<input type="text" value="<?php echo get_search_query() ?>" name="s" placeholder="Search this site" aria-label="Search" title="Search"/>
-    <button type="submit"><i class="fa fa-search"></i><span>Search</span></button>
+    <button type="submit" aria-label="Submit search"><i class="fa fa-search"></i><span>Search</span></button>
   </div>
 </form>
